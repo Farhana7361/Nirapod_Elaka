75f6695cf6c2bdcd79253847e8b5322ae981be11
Farhana7361 <farhanarahman7361@gmail.com>
2026-08-16 01:16:50 +0600

    NavFix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { NavLink } from 'react-router';
 
 export default function Navbar() {
   return (
-    <nav className="navbar bg-base-100 shadow-sm px-6">
+    <nav className="navbar fixed top-0 left-0 w-full z-50 bg-base-100 shadow-sm px-6">
       <div className="flex-1 flex items-center gap-2">
   <span className="w-2.5 h-2.5 rounded-full bg-amber-400 shadow-[0_0_8px_theme(colors.amber.400)]"></span>
   <NavLink to="/" className="text-xl font-bold">Nirapod Elaka</NavLink>
